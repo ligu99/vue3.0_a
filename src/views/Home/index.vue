ac67b6f1ffc8eafec6f55f0d9a9c8f8bfe4fdcba
Laipili <ligu1005@gmail.com>
2020-12-08 14:52:42 +0800

    添加layout

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="index">home</div>
+  <div>
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout>
+        <a-layout-header>Header</a-layout-header>
+        <a-layout-content>Content</a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </div>
 </template>
 
 <script>
+// import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons-vue';
 export default {
+  // components: {
+  //   UserOutlined,
+  //   VideoCameraOutlined,
+  //   UploadOutlined,
+  // },
+  data() {
+    return {
 
+    };
+  },
+  methods: {
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-.index {
-  position: relative;
-}
+<style>
 </style>
