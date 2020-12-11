46248fb7974a1057809c9a3afa3fdd5c69bb880d
Laipili <ligu1005@gmail.com>
2020-12-11 18:54:31 +0800

    1211

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -12,29 +12,11 @@
 </template>
 
 <script>
-import { getApi } from "@/api/base";
 export default {
   setup() {
-    let click = () => {
-      let url = "/v1/s/q04sv38ZSzTBF4mEHo+3dVsyWITNHb2AHhoKmmcfMQvFLURsx3V4aUR26W8PQUqCSaa+Jm++DRZ9OBs55XAJDQyzq0eJOMiIR1DqFyeHkjiFz/TPP8FfRddLJaCyZHeBqfEnQtelvIQ="
-      getApi({
-        url: url
-        /**
-         * 相当于请求http://localhost:8080/v1/portal/menu/GetTree?domain=agent
-         * 由于设置了代理
-         * 最终请求的是https://wwwtest.ctyun.cn:21443/v1/portal/menu/GetTree?domain=agent
-        */
-
-      }).then(data => {
-        console.log(data)
-      }).catch(err => {
-        console.log(err)
-      })
-    }
 
     return {
-      click
-    }
+    };
   }
 }
 </script>
