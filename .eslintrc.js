6e2385cc2fa921f2775d197e4f0c817c08b5ef16
ligu99 <ligu1005@gmail.com>
2021-03-05 10:43:17 +0800

    修改检验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "generator-star-spacing": "off",
+    "no-tabs": "off",
+    "no-unused-vars": "off",
+    "no-console": "off",
+    "no-irregular-whitespace": "off",
+    "no-debugger": "off"
   }
 };
